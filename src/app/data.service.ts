bc9ed66258e86b736b7514977745ec7a49ae66f7
Situ William <situ_william@yahoo.com>
2019-10-25 01:27:51 -0400

    chore: cleaned up some unused code and added new assets

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class DataService {
-	private goals = new BehaviorSubject<any>(['my first life goal', 'i want to make people happy', 'get a girlfriend']);
+	private goals = new BehaviorSubject<any>([]);
 	goal = this.goals.asObservable();
 
   	constructor() { }
